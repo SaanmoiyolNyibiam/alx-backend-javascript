909c7fa666528937c758435ad5dd30402d7b6b83
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-01 00:47:14 +0100

    build(alx-b-js): adjust taskBlock function to meet task requirements

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    var task = true;
+    var task2 = false;
 }
+
+  var task = false;
+  var task2 = true;
 
   return [task, task2];
 }
