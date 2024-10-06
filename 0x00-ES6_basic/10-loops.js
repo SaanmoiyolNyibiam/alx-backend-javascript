e2b192e27f71752da4f1f35da8582b66e779fd09
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-06 05:35:24 +0100

    build(alx-b-js:0x00): add solution to task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
+  const newArray = array;
+  for (const value of array) {
+    newArray[i] = appendString + value;
+    i += 1;
+  }
+
+  return array;
+}
