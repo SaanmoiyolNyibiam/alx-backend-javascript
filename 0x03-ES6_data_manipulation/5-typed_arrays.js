9f82950232c9685b92d5773d69702a07e23d94b3
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-08 12:26:31 +0100

    build(alx-b-js:0x03): refactor createIntTyepedArray function's check statements and code to conform to es6 standards

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-    if (position >= length) throw new RangeError('Position outside range');
-    const newTypedArray = new Int8Array(length);
-    newTypedArray[position] = value;
+  if (position >= length) throw new RangeError('Position outside range');
+  if (value < -127 || value > 127) throw new RangeError('Value outside range for Int8TypedArray');
+  const newTypedArray = new Int8Array(length);
+  newTypedArray[position] = value;
 
-    return newTypedArray;
+  return newTypedArray.buffer;
 }
