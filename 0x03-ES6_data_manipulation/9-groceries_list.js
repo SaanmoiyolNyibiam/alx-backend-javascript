860eb12b25e527149b56cea8801b482a30980d79
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-09 00:18:18 +0100

    build(alx-b-js:0x03): add solution to task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,7 @@
+export default function groceriesList() {
+  // create new map from an array of arrays
+  const newMap = new Map([['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]]);
+
+  // return new map created
+  return newMap;
+}
