c68959b1ffdea186d7eed2c38622f9885bd79da8
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-08 12:55:24 +0100

    build(alx-b-js:0x03): refactor createInt8TypedArray to use dataview for managing the buffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,14 @@
 export default function createInt8TypedArray(length, position, value) {
+  // validate function input
   if (position >= length) throw new RangeError('Position outside range');
   if (value < -127 || value > 127) throw new RangeError('Value outside range for Int8TypedArray');
-  const newTypedArray = new Int8Array(length);
-  newTypedArray[position] = value;
 
-  return newTypedArray.buffer;
+  // create the buffer and the typedArray
+  const buffer = new ArrayBuffer(length);
+  const data = new DataView(buffer);
+
+  // set the value at the specified position
+  data.setInt8(position, value);
+
+  return data;
 }
