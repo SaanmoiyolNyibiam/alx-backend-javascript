a1a16e2e90987d0b8561c54996ec5b5437e0eb47
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-08 15:27:18 +0100

    build(alx-b-js:0x03): add solution to task 7

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,12 @@
+export default function hasValuesFromArray(inputSet, inputArray) {
+  // validate input
+  if (!Array.isArray(inputArray) || !(inputSet instanceof Set)) throw new TypeError('Invalid input types');
+
+  // check if all elements in array exist in set
+  // const boolList = inputArray.map((arrayValue) => inputSet.has(arrayValue));
+  const bool = inputArray.every((arrayValue) => inputSet.has(arrayValue));
+
+  // return boolean
+  console.log(bool);
+  // return bool;
+}
