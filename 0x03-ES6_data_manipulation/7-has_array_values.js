e597f24899b96f85678c1388895a19fb87243119
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-09 05:42:35 +0100

    style(alx-b-js:0x03): format file to conform with es6 specifications

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -3,7 +3,6 @@ export default function hasValuesFromArray(inputSet, inputArray) {
   if (!Array.isArray(inputArray) || !(inputSet instanceof Set)) throw new TypeError('Invalid input types');
 
   // check if all elements in array exist in set
-  // const boolList = inputArray.map((arrayValue) => inputSet.has(arrayValue));
   const bool = inputArray.every((arrayValue) => inputSet.has(arrayValue));
 
   // return boolean
