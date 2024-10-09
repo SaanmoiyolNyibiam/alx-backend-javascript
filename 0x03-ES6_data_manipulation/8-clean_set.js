e16194dc878975caebd2e72afa7381c37727af93
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-09 05:49:34 +0100

    refactor(alx-b-js:0x03): change entire algorithm for cleadSet function

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,22 @@
-export default function cleanSet(inputSet, startString) {
-  // validate input
-  if (!(inputSet instanceof Set) || typeof startString !== 'string') throw new TypeError('Invalid input type');
-  if (startString.length === 0 || startString === undefined) return '';
+export default function cleanSet(set, string) {
+  // // validate input
+  // if (!(inputSet instanceof Set) || typeof startString !== 'string') {
+  // throw new TypeError('Invalid input type');
+  // if (startString.length === 0 || startString === undefined) return '';
 
-  // check for startString in values of inputSet and compute a string of matching values
-  // for (const value of inputSet) {
-  //   if (value.startsWith(startString)) {
-  //     outputString += `${value.replace(startString, '')}-`;
-  //   }
-  // }
-  // // return the string of matching value
-  // return outputString.slice(0, -1);
+  // const outputString = [...inputSet]
+  //   .filter((setValue) => (typeof setValue === 'string' && setValue
+  // .startsWith(startString) && setValue !== undefined))
+  //   .map((eachString) => (eachString !== undefined ? eachString.slice(startString.length) : ''))
+  //   .join('-');
 
-  const outputString = [...inputSet]
-    .filter((setValue) => (typeof setValue === 'string' && setValue.startsWith(startString) && setValue !== undefined))
-    .map((eachString) => (eachString !== undefined ? eachString.slice(startString.length) : ''))
+  // return outputString;
+
+  if (string === undefined || string.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((str) => (str !== undefined ? str.startsWith(string) : ''))
+    .map((str) => (str !== undefined ? str.slice(string.length) : ''))
     .join('-');
-
-  return outputString;
 }
