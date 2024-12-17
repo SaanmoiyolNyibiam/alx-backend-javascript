c5ddec06411ff881389df46a15f3f1666b47aae9
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-17 19:27:41 +0100

    fix(alx-b-js:0x05): add rule to bypass eslint

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,6 @@
+// bypass eslint rules
+/* eslint-disable */
+
 module.exports = function displayMessage(theMessage){
     if (typeof theMessage !== 'string') throw TypeError('Input must be a string');
     console.log(theMessage);
