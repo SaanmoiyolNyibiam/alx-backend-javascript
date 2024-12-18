54536f31b007b098d877df9f2b0c9204030287ff
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-18 19:26:06 +0100

    build(alx-b-js:0x01): add solution to task 0

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+// defines function that returns a promise
+export default function getResponseFromAPI() {
+  const promise = new Promise((resolve, reject) => {
+    const Success = true;
+    if (Success) {
+      resolve('Operation successful');
+    } else {
+      reject(new Error('Operation failed'));
+    }
+  });
+  return promise;
+}
