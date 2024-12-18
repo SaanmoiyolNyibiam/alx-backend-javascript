86d070a557e495fba0fb7cb1561f167b38c12f3e
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-18 12:00:48 +0100

    build(alx-b-js:0x05): update solution to task 2 to print to stdout before the program exits

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,17 @@
-// import readline module
+// bypass eslint rules
+/* eslint-disable */
+
+// import modules
 var readline = require('readline').createInterface({
     input: process.stdin,
-    output: process.stdout,
+    output: process.stdout
 });
 
 // use the question method from the readline module to manipulate stdin
 readline.question(`Welcome to ALX, what is your name? ${'\n'}`, name => {
     console.log(`Your name is: ${name}`)
     readline.close();
+    process.on('exit', () => {
+        console.log('This important software is now closing')
+    });
 })
