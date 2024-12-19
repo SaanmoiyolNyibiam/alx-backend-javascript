ab592cf7a465d3947bb414cfb0b0ce7879419136
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-19 22:04:27 +0100

    refactor(alx-b-js:0x01): update code logic to meet checker requirements

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,5 +6,15 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   // calls the two imported functions and when settled returns an array
   const newUser = signUpUser(firstName, lastName);
   const newPhoto = uploadPhoto(filename);
-  return Promise.allSettled([newUser, newPhoto]).then((res) => res);
+  return Promise.allSettled([newUser, newPhoto]).then((res) => {
+    const result = {};
+    res.forEach((idv) => {
+      if (idv.status === 'fulfilled') {
+        result[idv.status] = idv.value;
+      } else {
+        result[idv.status] = idv.reason;
+      }
+    });
+    return result;
+  });
 }
