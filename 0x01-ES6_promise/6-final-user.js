ee6fbf050ec83ab3710caa3fccf36cae768f0e08
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-19 21:42:18 +0100

    refactor(alx-b-js:0x01): use Promise.allSettled instead of Promise.all

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   // calls the two imported functions and when settled returns an array
   const newUser = signUpUser(firstName, lastName);
   const newPhoto = uploadPhoto(filename);
-  return Promise.all([newUser, newPhoto]).then((res) => {
+  return Promise.allSettled([newUser, newPhoto]).then((res) => {
     console.log(res);
   });
 }
