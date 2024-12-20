a4efdf0a24a31c9550a8da27fab073a7b1ad711b
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-20 11:17:36 +0100

    build(alx-b-js:0x01): add solution to task 6 again

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+// this is a module that handleProfileSignup function
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  // calls the two imported functions and when settled returns an array
+  const newUser = signUpUser(firstName, lastName);
+  const newPhoto = uploadPhoto(filename);
+
+  return Promise.allSettled([newUser, newPhoto]).then((res) => {
+    const result = [];
+    res.forEach((idv) => {
+      if (idv.status === 'fulfilled') {
+        result.push({ status: idv.status, value: idv.value });
+      } else {
+        result.push({ status: idv.status, value: idv.reason });
+      }
+    });
+    return result;
+  });
+}
