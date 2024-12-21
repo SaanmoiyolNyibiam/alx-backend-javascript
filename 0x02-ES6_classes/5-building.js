cace80887791885ff992dc0c9168245ad9de357b
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-21 21:31:13 +0100

    build(alx-b-js:0x02): add solution to task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+// this is a module that defines a building class
+
+export default class Building {
+  constructor(sqftInput) {
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this.sqft = sqftInput;
+  }
+
+  // sets sqft
+  set sqft(sqftInput) {
+    if (typeof sqftInput !== 'number') throw TypeError('sqft must be a number');
+    this._sqft = sqftInput;
+  }
+
+  // gets sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // an abstract method
+  evacuationWarningMessage() {
+  }
+}
