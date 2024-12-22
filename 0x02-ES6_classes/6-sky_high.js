d930bdde297d510eaf35252a6fc9d4186c43b90c
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-22 11:45:27 +0100

    style(alx-b-js:0x02): update code to meet eslint specifications

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,25 +1,25 @@
-import Building from "./5-building";
+import Building from './5-building';
 // this is a module that defines a skyHighBuilding class
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqftInput, floorsInput) {
-        super(sqftInput);
-        this.floors = floorsInput;
-    }
+  constructor(sqftInput, floorsInput) {
+    super(sqftInput);
+    this.floors = floorsInput;
+  }
 
-    // sets the number of floors
-    set floors(floorsInput) {
-        if (typeof floorsInput !== 'number') throw TypeError('floorsInput must be a number');
-        this._floors = floorsInput;
-    }
+  // sets the number of floors
+  set floors(floorsInput) {
+    if (typeof floorsInput !== 'number') throw TypeError('floorsInput must be a number');
+    this._floors = floorsInput;
+  }
 
-    // gets the number of floors
-    get floors() {
-        return this._floors;
-    }
+  // gets the number of floors
+  get floors() {
+    return this._floors;
+  }
 
-    // overides the original function in the base class
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors`;
-    }
+  // overides the original function in the base class
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
