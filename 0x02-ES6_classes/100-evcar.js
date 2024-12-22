9d590e31518a2f48f08953dafddd8d54a74a41c0
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-22 17:44:08 +0100

    build(alx-b-js:0x02): add solution to task 11

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+import Car from './10-car';
+// this is a module that defines an EVCar class
+
+export default class EVCar extends Car {
+  constructor(brandInput, motorInput, colorInput, rangeInput) {
+    super(brandInput, motorInput, colorInput);
+    this._range = rangeInput;
+  }
+
+  // overrides the clone method for the base class
+  cloneCar() {
+    return new Car();
+  }
+}
