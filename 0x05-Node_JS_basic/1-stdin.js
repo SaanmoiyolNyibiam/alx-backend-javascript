a6b8c1564dee2c0a90ce4f6a4c251f9c916053ea
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-29 16:21:16 +0100

    fix(alx-b-js:0x05): log string to stdout instead of objects

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,17 +4,17 @@ process.stdout.write('Welcome to ALX, what is your name?\n');
 if (process.stdin.isTTY) {
   // write the users name to stdout
   process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data}`);
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
 } else {
   // write the users name to stdout
   process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data}`);
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
   // handle closing of the program
   process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n')
+    process.stdout.write('This important software is now closing\n');
   });
 }
