c1b69c3a41cfad2ea20ab2328b314aa88fd5674f
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-29 16:02:55 +0100

    refactor(alx-b-js:0x05): handle input from pipe and terminal differently

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,20 @@
 // write string to welcome user
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
-// write the users name to stdout
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
-  process.exit();
-});
-
-// handle closing of the program
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+if (process.stdin.isTTY) {
+  // write the users name to stdout
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
+    process.exit();
+  });
+} else {
+  // write the users name to stdout
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
+    process.exit();
+  });
+  // handle closing of the program
+  process.on('exit', () => {
+    console.log('This important software is now closing');
+  });
+}
