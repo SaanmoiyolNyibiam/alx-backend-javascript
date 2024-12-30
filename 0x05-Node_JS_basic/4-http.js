e3a3d4c947444b9638c28c6c3daba6e8f1a7502f
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-30 19:23:36 +0100

    fix(alx-b-js:0x05): change text output for server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -9,7 +9,7 @@ Displays Hello ALX! in the page body for any endpoint as plain text
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello ALX!');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245, () => {
