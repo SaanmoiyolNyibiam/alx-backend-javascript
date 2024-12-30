2166055cbb0ce3bd495652cd0b0b7d5ab5ed53c3
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2024-12-30 17:56:04 +0100

    refactor(alx-b-js:0x05): update code algorithm

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -11,48 +11,29 @@ with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNA
 CSV file can contain empty lines (at the end) - and they are not a valid student!
 */
 
-function printList(inputList) {
-  // prints a lisst in a specified format
-  let formatted = '';
-
-  for (let idx = 0; idx < inputList.length; idx += 1) {
-    if (idx === inputList.length - 1) {
-      formatted += `${inputList[idx]}\n`;
-    } else {
-      formatted += `${inputList[idx]}, `;
-    }
-  }
-
-  return formatted;
-}
-
 module.exports = function countStudents(filePath) {
   // this is a function that reads a reads a file
 
   try {
     const fileOutput = fs.readFileSync(filePath, 'utf8');
     const rows = fileOutput.split('\n');
-    const records = rows.slice(1).map((idxVal) => idxVal.split(','));
+    const records = rows.slice(1).filter((row) => row.trim() !== '').map((idxVal) => idxVal.split(','));
 
-    const CSList = { course: 'CS', CScount: 0, fnames: [] };
-    const SWEList = { course: 'SWE', SWEcount: 0, fnames: [] };
-    const fieldList = { CSList, SWEList };
-
-    for (const value of records) {
-      if (value[value.length - 1] === 'CS') {
-        CSList.CScount += 1;
-        CSList.fnames.push(value[0]);
-      } else if (value[value.length - 1] === 'SWE') {
-        SWEList.SWEcount += 1;
-        SWEList.fnames.push(value[0]);
+    const fieldList = {};
+    records.forEach((value) => {
+      const firstname = value[0];
+      const course = value[value.length - 1];
+      if (!fieldList[course]) {
+        fieldList[course] = { count: 0, names: [] };
       }
-    }
+      fieldList[course].count += 1;
+      fieldList[course].names.push(firstname);
+    });
 
     console.log(`Number of students: ${records.length}`);
-    const List1 = printList(fieldList.CSList.fnames);
-    const List2 = printList(fieldList.SWEList.fnames);
-    process.stdout.write(`Number of students in ${fieldList.CSList.course}: ${fieldList.CSList.CScount}. List: ${List1}`);
-    process.stdout.write(`Number of students in ${fieldList.SWEList.course}: ${fieldList.SWEList.SWEcount}. List: ${List2} `);
+    for (const [course, data] of Object.entries(fieldList)) {
+      console.log(`Number of students in ${course}: ${data.count}. List: ${data.names.join(', ')}`);
+    }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
